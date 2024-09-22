4fc0756bb0e47c883e92553a16da2a7af2a47ac8
Pratham Dangol <109843656+dangol429@users.noreply.github.com>
2024-09-22 12:11:11 +0530

    added endpoint vercel

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ecommerce-website-2225.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
